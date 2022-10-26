84e3a752b5f005c8a923d49b84eda131872e7544
Jason Jensen <jj@chirocat.com>
2022-10-25 18:30:53 -0600

    Try again

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -25,11 +25,12 @@ export default function Register() {
           </div>
         </div>
         <form
-          name="contact-form"
+          name="contact"
           className="mt-10 grid grid-cols-1 gap-y-8 gap-x-6 sm:grid-cols-2"
-          netlify
           method="POST"
         >
+          <input type="hidden" name="form-name" value="contact" />
+
           <TextField
             label="First name"
             id="first_name"
